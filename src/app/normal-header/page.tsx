7ab14adc26e79a7452d67b0c5031562e1e75dcae
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-09 12:56:10 +0900

    fix: navigationを追加

--- src/app/normal-header/page.tsx
+++ src/app/normal-header/page.tsx
@@ -1,12 +1,14 @@
 import { Footer } from '@/app/normal-header/footer';
 import { Header } from '@/app/normal-header/header';
 import { Main } from '@/app/normal-header/main';
+import { Navigation } from '@/app/normal-header/navigation';
 import styles from './styles.module.css';
 
 export default function NomalHeaderHome() {
   return (
     <div className={styles.root}>
       <Header />
+      <Navigation />
       <Main />
       <Footer />
     </div>
